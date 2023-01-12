d2ec551220c05d17f34d5bc4832a309f007ad6a4
dstutman <git@stutman.tech>
2023-01-12 19:52:56 +0100

    Insert dummy loop

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,8 @@
 #include "trajectories.hpp"
 #include "executor.hpp"
 
-int main() {
+int main()
+{
     // Parameters
     const auto camera_effector_mass = 1.0;
     const std::array<double, 3UL> camera_effector_mass_offset = {0., 0., 0.05};
@@ -26,7 +27,7 @@ int main() {
     // Reset the robot
     robot.automaticErrorRecovery();
     robot.setLoad(camera_effector_mass, camera_effector_mass_offset, camera_effector_inertia);
-    
+
     // Unlock all joints and seek the bounding box
     robot.setGuidingMode({true, true, true, true, true, true}, true);
     std::cout << "Line the effector up with the base of the satellite and hit enter\n";
@@ -40,11 +41,26 @@ int main() {
     auto effector_pose = Eigen::Matrix4<double>(state.O_T_EE.data());
     auto effector_orientation = effector_pose.block<3, 3>(0, 0);
     auto effector_position = effector_pose.block<3, 1>(3, 0);
-    auto line = Trajectories::line_generator(Eigen::Vector3d(), Eigen::Vector3d(), Eigen::Quaterniond());
 
     // Determine Dephi's COM location in the base frame
     Eigen::Vector3<double> delfi_com_offset = {0, 0, 0.25}; // FIXME: Offset from base of Delfi to COM
     auto delfi_position = effector_position + effector_orientation.inverse() * delfi_com_offset;
+
+    // Execute the trajectory
+    const auto initial_time = std::chrono::system_clock::now();
+    const auto exec_config = Executor::ExecutorConfiguration{
+        trajectory : Trajectories::line_generator(Eigen::Vector3d(0., 0., 0.), Eigen::Vector3d(1., 0., 0.), Eigen::Quaterniond()),
+        trajectory_duration : 60.0,
+    };
+    auto exec_state = Executor::ExecutorState{
+        // NOTE: Narrowing ok if trajectory time sufficiently short
+        t : (double)initial_time.time_since_epoch().count(),
+    };
+    while (Executor::loop(exec_config, &exec_state, &robot))
+    {
+        const auto time_delta = std::chrono::system_clock::now() - initial_time;
+        exec_state.t = time_delta.count();
+    }
 
     robot.stop();
 }
