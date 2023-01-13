b2d230f8b0814e2cc91a429e1f9217d97afa4174
dstutman <git@stutman.tech>
2023-01-13 16:21:56 +0100

    Touchup

--- src/executor.cpp
+++ src/executor.cpp
@@ -2,7 +2,6 @@
 
 namespace Executor
 {
-
      Executor executor_generator(const ExecutorConfiguration exec_config, ExecutorState *const exec_state) {
         return [exec_config, exec_state](const franka::RobotState & robot_state, franka::Duration duration) -> franka::CartesianPose {
             exec_state->t += duration.toSec();
