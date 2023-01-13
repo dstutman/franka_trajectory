c374353109e3243611a1b7613fdea15d41a112bc
dstutman <git@stutman.tech>
2023-01-13 16:21:34 +0100

    Begin camera implementation

---
+++ include/camera.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "GxIAPI.h"
+
+namespace Camera {
+    void camera();
+}
---
+++ src/camera.cpp
@@ -0,0 +1,18 @@
+#include "camera.hpp"
+
+void camera() {
+    assert(GXInitLib() == GX_STATUS_SUCCESS);
+
+    uint32_t num_devices = 0;
+    GXUpdateDeviceList(&num_devices, 1000);
+
+    GX_OPEN_PARAM open_params;
+    open_params.accessMode = GX_ACCESS_EXCLUSIVE;
+    open_params.openMode = GX_OPEN_INDEX;
+    open_params.pszContent = (char*)"1";
+
+    GX_DEV_HANDLE device_handle;
+
+    assert(GXOpenDevice(&open_params, &device_handle) == GX_STATUS_SUCCESS);
+    GX
+}
