1856d73fd7c345c4b5ba1a8ea518564752c609d9
dstutman <git@stutman.tech>
2023-01-17 12:03:28 +0100

    Break camera into object

--- include/camera.hpp
+++ include/camera.hpp
@@ -3,5 +3,21 @@
 #include "GxIAPI.h"
 
 namespace Camera {
+    typedef enum {
+        CAMERA_STATUS_SUCCESS,
+        CAMERA_STATUS_FAILURE,
+    } CAMERA_STATUS;
+
+    class Camera {
+        protected:
+            GX_DEV_HANDLE _device;
+            int64_t _payload_size;
+
+
+        public:
+            Camera();
+            ~Camera();
+            CAMERA_STATUS capture_to_file(const char *file_name);
+    };
     void camera();
 }
